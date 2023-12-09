0c7b2feebcd6d84f741ef5630ff8db997680937f
Dev Reimu <github.543ov@slmails.com>
2023-12-09 20:29:36 +0200

    Update arduino.ino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,26 +1,17 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 
-const char *ssid = "UrbanDevCrew Distress Hotspot";
-const char *password = "";
+String httpRequest;
 
 ESP8266WebServer server(80);
-
-void handleRoot() {
-  server.send(200, "text/plain", "Welcome to UrbanDevCrew Distress Hotspot!");
-}
 
 void setup() {
   Serial.begin(115200);
 
-  // Connect to Wi-Fi
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");
+  pinMode(2, OUTPUT);
+
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP("UrbanDevCrew Distress Hotspot");
 
   // Set up web server
   server.on("/", HTTP_GET, handleRoot);
@@ -29,4 +20,18 @@ void setup() {
 
 void loop() {
   server.handleClient();
+}
+
+void handleRoot() {
+  // HTML with IP in DOM
+  //String htmlDocument = "";
+  //String ipAddress = WiFi.localIP().toString();
+  //htmlDocument.replace("IP_ADDRESS", ipAddress);
+  Serial.println(WiFi.softAPIP());
+  
+  digitalWrite(2, HIGH);
+  delay(3000);
+  digitalWrite(2, LOW);
+  
+  server.send(200, "text/html", "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><style>body{padding:0;margin:0;display:flex;justify-content:center}#message-container{width:80%;max-width:1200px}#message-container div{background-color:#ccc;padding:5px}#message-container div:nth-child(2n){background-color:#fff}#send-container{position:fixed;padding-bottom:30px;bottom:0;background-color:#fff;max-width:1200px;width:80%;display:flex}#message-input{flex-grow:1}</style></head><body><div id=\"message-container\"></div><form id=\"send-container\"><input type=\"text\" id=\"message-input\"><button type=\"submit\" id=\"send-button\">Send</button></form></body></html>");
 }
