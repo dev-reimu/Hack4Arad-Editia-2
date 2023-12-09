2778ba0d820693217f75c07e8dc55da1c2b4e776
Dev Reimu <github.543ov@slmails.com>
2023-12-09 21:50:23 +0200

    Update arduino.ino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,17 +1,20 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include <DNSServer.h> 
 
-String httpRequest;
+const char *ssid = "UrbanDevCrew Distress Hotspot";
 
 ESP8266WebServer server(80);
 
 void setup() {
   Serial.begin(115200);
 
-  pinMode(2, OUTPUT);
+  // Access point
+  WiFi.mode(WIFI_AP_STA);
+  WiFi.softAP(ssid);
 
-  WiFi.mode(WIFI_AP);
-  WiFi.softAP("UrbanDevCrew Distress Hotspot");
+  // Attempt to connect to other similar Access Points
+  WiFi.begin(ssid);
 
   // Set up web server
   server.on("/", HTTP_GET, handleRoot);
@@ -20,18 +23,20 @@ void setup() {
 
 void loop() {
   server.handleClient();
+  findOtherAPs();
 }
 
 void handleRoot() {
-  // HTML with IP in DOM
-  //String htmlDocument = "";
-  //String ipAddress = WiFi.localIP().toString();
-  //htmlDocument.replace("IP_ADDRESS", ipAddress);
   Serial.println(WiFi.softAPIP());
-  
-  digitalWrite(2, HIGH);
-  delay(3000);
-  digitalWrite(2, LOW);
-  
-  server.send(200, "text/html", "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><style>body{padding:0;margin:0;display:flex;justify-content:center}#message-container{width:80%;max-width:1200px}#message-container div{background-color:#ccc;padding:5px}#message-container div:nth-child(2n){background-color:#fff}#send-container{position:fixed;padding-bottom:30px;bottom:0;background-color:#fff;max-width:1200px;width:80%;display:flex}#message-input{flex-grow:1}</style></head><body><div id=\"message-container\"></div><form id=\"send-container\"><input type=\"text\" id=\"message-input\"><button type=\"submit\" id=\"send-button\">Send</button></form></body></html>");
+  server.send(200, "text/html", "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><title>UrbanDev Crew</title><style>body {padding: 0;margin: 0;display: flex;justify-content: center;}#message-container {width: 80%;max-width: 1200px;}#message-container div {background-color: #CCC;padding: 5px;}#message-container div:nth-child(2n) {background-color: #FFF;}#send-container {position: fixed;padding-bottom: 30px;bottom: 0;background-color: white;max-width: 1200px;width: 80%;display: flex;}#message-input {flex-grow: 1;}</style></head><body><div id=\"message-container\"></div><form id=\"send-container\"><input type=\"text\" id=\"message-input\"><button type=\"submit\" id=\"send-button\">Send</button></form><script>const name = prompt('What is your name?')const messageContainer = document.getElementById('message-container')const messageForm = document.getElementById('send-container')const messageInput = document.getElementById('message-input')messageForm.addEventListener('submit', e => {e.preventDefault()const message = messageInput.valueappendMessage(You: ${message})messageInput.value = ''return false; // Prevent form submission})function appendMessage(message) {const messageElement = document.createElement('div')messageElement.innerText = messagemessageContainer.append(messageElement)}appendMessage('You joined')</script></body></html>");
+}
+
+void findOtherAPs() {
+  if (WiFi.status() != WL_CONNECTED) {
+    delay(5000);
+    Serial.println("Searching for other UrbanDevCrew access points nearby...");
+    return;
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
 }
