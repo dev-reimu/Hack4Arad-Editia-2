7226c73e0f9f843bc30931269aa0cb6da8c59a71
TheGamerXDD <130509141+TheGamerXDD@users.noreply.github.com>
2023-12-09 14:03:09 +0200

    Update arduino.ino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,12 +1,33 @@
-#include <WiFi.h>
-#include <WiFiClient.h>
-#include <WiFiServer.h>
-#include <WiFiUdp.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+const char *ssid = "YourSSID";
+const char *password = "YourPassword";
+
+ESP8266WebServer server(80);
+
+void handleRoot() {
+  server.send(200, "text/plain", "Hello from Arduino!");
+}
 
 void setup() {
-  
+  Serial.begin(115200);
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(250);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+
+  // Set up web server
+  server.on("/", HTTP_GET, handleRoot);
+  server.begin();
 }
 
 void loop() {
-  
+  server.handleClient();
 }
