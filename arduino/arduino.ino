3a342c971646f3ea10567d9f7dd52fba6ecb0aaf
Dev Reimu <github.543ov@slmails.com>
2023-12-09 14:22:54 +0200

    Update arduino.ino

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,13 +1,13 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 
-const char *ssid = "UrbanDev-Ard";
+const char *ssid = "UrbanDevCrew Distress Hotspot";
 const char *password = "";
 
 ESP8266WebServer server(80);
 
 void handleRoot() {
-  server.send(200, "text/plain", "Hello from Arduino!");
+  server.send(200, "text/plain", "Welcome to UrbanDevCrew Distress Hotspot!");
 }
 
 void setup() {
@@ -16,10 +16,9 @@ void setup() {
   // Connect to Wi-Fi
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
-    delay(250);
+    delay(1000);
     Serial.print(".");
   }
-
   Serial.println("");
   Serial.println("WiFi connected");
 
